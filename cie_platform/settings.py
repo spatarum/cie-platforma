c048297d6fda9b887874482d49cf14f7f43e013e
spatarum <spatarum@gmail.com>
2026-01-29 21:27:22 +0200

    Update settings.py

--- cie_platform/settings.py
+++ cie_platform/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
