91eb7b6ac9c0599cb28b15f8272e9c5e88bf7894
spatarum <spatarum@gmail.com>
2026-01-31 12:40:29 +0200

    Add files via upload

--- portal/urls.py
+++ portal/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="portal/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
 
     path("", views.home, name="home"),
 
